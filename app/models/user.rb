cb4be8ade8df98df6cac3b0f8c959181284b95cc
DawnBringsABloomingARose <113314828+DawnBringsABloomingRose@users.noreply.github.com>
2023-08-23 19:37:07 -0400

    Add a friend request association, allowing users to send and recieve friend requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  has_many :sent_requests, foreign_key: "sender_id", class_name: "FriendRequest"
+  has_many :recieved_requests, foreign_key: "reciever_id", class_name: "FriendRequest"
+  #has_many :recieved_requests, through: :sent_requests, source: :reciever
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_201006) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_225504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.bigint "sender_id", null: false
+    t.bigint "reciever_id", null: false
+    t.boolean "seen", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reciever_id"], name: "index_friend_requests_on_reciever_id"
+    t.index ["sender_id"], name: "index_friend_requests_on_sender_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,4 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_201006) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "friend_requests", "users", column: "reciever_id"
+  add_foreign_key "friend_requests", "users", column: "sender_id"
 end
